b8b692f722653133b4f9b9d98b618a16177266ef
Ewan Rakotoanosy <55093676+rakotoew@users.noreply.github.com>
2023-08-16 23:37:21 +0200

    modify hero section

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -12,10 +12,13 @@ export default function Hero(): JSX.Element {
                 lg:ml-12
                 xl:ml-14"
                 >
-                    <Image className="rounded-full absolute" src="/profile.png" alt="profile picture" width="250" height="250" />
-                    <div className="p-3 pl-32 bg-gradient-to-r to-gray-800 from-gray-700 rounded-2xl ml-32">
+                    <Image className="rounded-full absolute max-w-[40%] md:max-w-[50%] lg:max-w-[80%] xl:max-w-[100%]" src="/profile.png" alt="profile picture" width={250} height={250} sizes="(max-width: 500px) 50vw"/>
+                    <div className="p-3 pl-20 min-[480px]:pl-32 bg-gradient-to-r to-gray-800 from-gray-700 rounded-2xl ml-10 min-[310px]:ml-20 min-[400px]:ml-28 min-[500px]:ml-32">
                         <h1
-                            className="text-gray-50 text-2xl
+                            className="text-gray-50
+                        text-base
+                        min-[400px]:text-xl
+                        min-[500px]:text-2xl
                         md:text-4xl
                         lg:text-5xl
                         xl:text-7xl"
@@ -23,7 +26,10 @@ export default function Hero(): JSX.Element {
                             {t("name")}
                         </h1>
                         <h3
-                            className="text-gray-200 text-xl
+                            className="text-gray-200
+                        text-sm
+                        min-[400px]:text-base
+                        min-[500px]:text-xl
                         md:text-2xl
                         lg:text-3xl
                         xl:text-4xl"
@@ -32,6 +38,9 @@ export default function Hero(): JSX.Element {
                         </h3>
                         <p
                             className="text-gray-400
+                        text-xs
+                        min-[400px]:text-sm
+                        min-[500px]:text-base
                         md:text-xl
                         lg:text-2xl
                         xl:text-3xl"
