a04f804defef7f9be956788dce327ba01a2bc116
Ewan Rakotoanosy <55093676+rakotoew@users.noreply.github.com>
2023-08-17 10:09:41 +0200

    feat(app): ajout header for XSS protection

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,14 +14,19 @@ export default function App({ Component, pageProps, router }: AppProps) {
                 <meta name="author" content="rakotoew" />
                 <meta property="og:type" content="website" />
                 <meta property="og:url" content="www.rakotoew.fr" />
-                <meta property="og:title" content="Ewan Rakotoanosy portfolio" />
-                <meta property="og:description" content="REPLACE_ME" />
+                <meta property="og:title" content="Ewan Rakotoanosy's portfolio" />
+                <meta property="og:description" content="Portfolio of Ewan Rakotoanosy, a FullStack developper" />
                 <meta property="og:image" content="/assets/banner.png" />
                 <meta property="twitter:card" content="summary_large_image" />
-                <meta property="twitter:url" content="REPLACE_ME" />
-                <meta property="twitter:title" content="REPLACE_ME" />
-                <meta property="twitter:description" content="REPLACE_ME" />
+                <meta property="twitter:url" content="Ewan Rakotoanosy's portfolio" />
+                <meta property="twitter:title" content="Ewan Rakotoanosy's portfolio" />
+                <meta property="twitter:description" content="Portfolio of Ewan Rakotoanosy, a FullStack developper" />
                 <meta property="twitter:image" content="/assets/banner.png" />
+
+                <meta httpEquiv={"Content-Type"} content={"text/html; charset=utf-8"} />
+                <meta httpEquiv={"X-Content-Type-Options"} content={"nosniff"} />
+                <meta httpEquiv={"X-Frame-Options"} content={"SAMEORIGIN"} />
+                <meta httpEquiv={"X-XSS-Protection"} content={"1; mode=block"} />
             </Head>
             <NextIntlProvider messages={pageProps.messages}>
                 <Component {...pageProps} key={router.route} />
